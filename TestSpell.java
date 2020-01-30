fccd8fcd6768596a8f6862eb21d7248107f20db2
Andres Flores <andres06037@gmail.com>
2020-01-30 13:14:28 -0600

    started creating characters

---
+++ TestSpell.java
@@ -0,0 +1,10 @@
+public class TestSpell
+{
+    public static void main(String[] args) 
+    {
+        Character character1 = new Character();
+        Character enemy = new Character(10, 10, "jacbo", "leroy", 100, 20);
+
+        
+    }
+}
