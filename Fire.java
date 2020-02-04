2bcbc3cc8f9163c73c175050ff6bc5d4d17a5681
Jacab Daves <genjacob20@gmail.com>
2020-02-04 13:19:08 -0600

    lots of edits

--- Fire.java
+++ Fire.java
@@ -4,9 +4,29 @@
 public class Fire extends Spell{
     public Fire(){
         super(20,"Fire",5);
+        SPELL_TYPE = "Fire Cantrip";
     }
-    public void chooseSpell(Spell Type){
+    @Override
+    public int cast(Character caster, Character victim){
+        int curMana = caster.getMana();
+        int reqMana = getManaReq();
+        int damage = getDmg();
+        if(caster.getMana() >= reqMana){
+            if(victim.isWeakTo(SPELL_TYPE)){
+                damage *=1.5;
+                victim.takeDamage((int)(damage));
+                caster.setMana(curMana-reqMana);
+            }
+            else{
+                victim.takeDamage(damage);
+                caster.setMana(curMana-reqMana);
+            }
+        }
+
+
+        return damage;
 
     }
+
 }
         
