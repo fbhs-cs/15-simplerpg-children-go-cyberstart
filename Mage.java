504ab74fc233d685c08640a6e657e3992e633214
Andres Flores <andres06037@gmail.com>
2020-02-04 14:10:02 -0600

    added correct constructor

--- Mage.java
+++ Mage.java
@@ -5,8 +5,9 @@ public class Mage extends Character{
     private Spell fire;
         
     
-    public Mage(){
-        super();
+    public Mage(int xp , String weakTo, String name, int speed) {
+        super(xp, weakTo, name, speed);
+        
     }
 
     public void chooseAction(Character target){
