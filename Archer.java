cf88d17514e594e7079d90bcc1a2327663b86c0f
Andres Flores <andres06037@gmail.com>
2020-02-04 14:10:29 -0600

    create class

---
+++ Archer.java
@@ -0,0 +1,8 @@
+public class Archer extends Character {
+    private Weapon weapon;
+
+    public Archer(int xp , String weakTo, String name, int speed) {
+        super(xp, weakTo, name, speed);
+        
+    }
+}
