c3aba9c96a326d05c56d56803d201d184f97dbbf
Andres Flores <andres06037@gmail.com>
2020-02-05 13:20:01 -0600

    added actions

--- Archer.java
+++ Archer.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
+
 public class Archer extends Character {
-    private Weapon weapon;
+    private Weapon bow;
 
     public Archer(int xp , String weakTo, String name, int speed) {
         super(xp, weakTo, name, speed);
+        bow = new Bow(30, "Piercing", 25);
         
+    }
+
+    @Override
+    public void chooseAction(Character target) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("What would " + this.getName() + " like to do?");
+        System.out.println("1. Attack \n2. Heal");
+        
+        String ans = s.nextLine();
+
+        if (ans.equals("1")) {
+            int dam = bow.attack(target);
+            System.out.println(this.getName() + "(" + this.getHp() + ")" + 
+                            " attacked " + target.getName() + "(" + target.getHp() + ")"
+                             + " with " + bow.getWeaponType() + " for " + dam + " damage");
+        }
+        else if (ans.equals("2")) {
+            int increase = this.getMaxHp();
+            this.heal((int)(increase * .25));
+        }
+
+        s.close();
+
+
     }
 }
--- Ghoul.java
+++ Ghoul.java
@@ -1,5 +1,17 @@
 public class Ghoul extends Character {
+    private Weapon bow = new Bow(30, "Piercing", 10);
+
     public Ghoul(int xp, String weak, String name, int howFast) {
         super(xp, weak, name, howFast);
+    }
+
+    @Override
+    public void chooseAction(Character target) {
+        int dam = bow.attack(target);
+        System.out.println(this.getName() + "(" + this.getHp() + ")" + 
+                            " attacked " + target.getName() + "(" + target.getHp() + ")"
+                             + " with " + bow.getWeaponType() + " for " + dam + " damage");
+            
+
     }
 }
