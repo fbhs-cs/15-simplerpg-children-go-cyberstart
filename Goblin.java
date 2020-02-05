f2ad0db0440434867278c2bee8be4bccb4e8e63a
Jacab Daves <genjacob20@gmail.com>
2020-02-05 12:59:13 -0600

    mage edited

--- Goblin.java
+++ Goblin.java
@@ -1,9 +1,12 @@
 public class Goblin extends Character{
-    private Weapon weapon;
+    private Weapon PoisonSword;
 
     public Goblin(int xp , String weakTo, String name, int speed) {
         super(xp, weakTo, name, speed);
         
+    }
+    public void chooseAction(Character target){
+        PoisonSword.attack(target);
     }
 
 
