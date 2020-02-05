a00ec41ddad3ea51011ed12b7ee9d382d41b51a0
unknown <marley549@icloud.com>
2020-02-04 21:44:45 -0600

    worked on skelly

--- Skeleton.java
+++ Skeleton.java
@@ -1,6 +1,6 @@
 public class Skeleton extends Character{
-    public Skeleton(int xp, String weakTo, String name){
-        super(66, "Axe", name);
+    public Skeleton(int xp, String weakTo, String name, int howFast){
+        super(66, "Axe", name, howFast);
     }
 
 }
