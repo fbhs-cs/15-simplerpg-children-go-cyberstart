c022d767222c3ff29eb77ed555853d6d853f4a19
Jacab Daves <genjacob20@gmail.com>
2020-02-06 13:18:49 -0600

    mage finished

--- Mage.java
+++ Mage.java
@@ -12,20 +12,22 @@ public class Mage extends Character{
     public void chooseAction(Character target){
         Scanner input = new Scanner(System.in);
         int health = getHp();
-        System.out.println("What do you want to do? Run(r), Cast(c) ");
+        System.out.println("What do you want to do? Run(r), Cast(c), and Heal(h). ");
         String action = input.next();
         if(action.equals("r")){
             setHp(health/2);
             System.out.println("You've run away but the enemy attacked you! Youve lost half your healh leaving you at" + getHp() +" left.");
         }
         else if(action.equals("c")){
-            this.cast(target);
-
-
-
+            Fire.cast(this, target);
+        }
+        else if(action.equals("h")){
+            int heals = this.getHp();
+            this.heal(heals/4);
         }
 
         input.close();
     }
 
+}
 }
