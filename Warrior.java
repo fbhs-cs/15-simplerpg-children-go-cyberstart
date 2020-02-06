7001d267f5e911f9a89e5950ff0171af7787aa41
marlena <marley549@icloud.com>
2020-02-06 13:19:24 -0600

    worked on warrior

--- Warrior.java
+++ Warrior.java
@@ -10,32 +10,37 @@ public class Warrior extends Character{
 
     public void chooseAction(Character target){
         Scanner input = new Scanner(System.in);
-        int health = getHp();
-        System.out.println("What do you want to do? (r)Run, (a)Attack or (h)Heal? ");
-        String action = input.next();
-        if(action.equals("a")||action.equals("A")){
-            PoisonSword.attack(target);
+        while(true){
+            int health = getHp();
+            System.out.println("What do you want to do? (r)Run, (a)Attack or (h)Heal? ");
+            String action = input.next();
+            if(action.equals("a")||action.equals("A")){
+                System.out.println();
+                PoisonSword.attack(target);
+                System.out.printf("%s attacked %s and it lost %d hp.", getName(), target.getName(),  );
+                System.out.println();
 
+
+            }
+            else if(action.equals("h")|| action.equals("H")){
+                int increase = this.getMaxHp();
+                System.out.println();
+                this.heal((int)(increase*.25));
+                System.out.println();
+
+
+
+            }
+            else if(action.equals("r")||action.equals("R")){
+                setHp(health/2);
+                System.out.println();
+                System.out.printf("You escaped barely surviving, only having %d hp left", this.getHp());
+                System.out.println();
+            }
+            break;
 
         }
-        if(action.equals("h")|| action.equals("H")){
-            int increase = this.getMaxHp();
-            this.heal((int)(increase*.25));
-
-
-
-        }
-        if(action.equals("r")||action.equals("R")){
-            setHp(health/2);
-        }
-
-
-
-
-
-
-
-
+        
         input.close();
 
     }
