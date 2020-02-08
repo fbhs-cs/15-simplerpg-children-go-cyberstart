10aa0e05578e8e88d858f3a4ac5e4cbddf758e1a
Andres Flores <andres06037@gmail.com>
2020-02-07 20:40:05 -0600

    added a weapon, and proper print statements

--- Goblin.java
+++ Goblin.java
@@ -1,12 +1,17 @@
 public class Goblin extends Character{
-    private Weapon PoisonSword;
+    private Weapon poisonSword;
 
     public Goblin(int xp , String weakTo, String name, int speed) {
         super(xp, weakTo, name, speed);
-        
+        poisonSword = new PoisonSword(25, "Poison", 10);
     }
+
+    @Override
     public void chooseAction(Character target){
-        PoisonSword.attack(target);
+        int dam = poisonSword.attack(target);
+        System.out.println(this.getName() + "(" + this.getHp() + ")" + 
+                            " attacked " + target.getName() + "(" + target.getHp() + ")"
+                             + " with " + poisonSword.getWeaponType() + " for " + dam + " damage");
     }
 
 
