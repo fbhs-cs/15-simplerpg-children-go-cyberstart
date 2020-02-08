df0e1872de80302bf04c4c2841c8ed83650dce9d
Andres Flores <andres06037@gmail.com>
2020-02-07 20:25:35 -0600

    implemented weapon

--- Warrior.java
+++ Warrior.java
@@ -2,10 +2,11 @@
 import java.util.*;
 
 public class Warrior extends Character{
-    private Weapon PoisonSword;
+    private Weapon poisonSword;
 
     public Warrior(int xp, String weakTo, String name, int howFast){
         super(xp, weakTo, name, howFast);
+        poisonSword = new PoisonSword(25, "Poison", 40);
     }
 
     public void chooseAction(Character target){
@@ -16,7 +17,7 @@ public class Warrior extends Character{
             String action = input.next();
             if(action.equals("a")||action.equals("A")){
                 System.out.println();
-                int dam = PoisonSword.attack(target);
+                int dam = poisonSword.attack(target);
                 System.out.printf("%s attacked %s and it lost %d hp.", getName(), target.getName(), dam);
                 System.out.println();
 
