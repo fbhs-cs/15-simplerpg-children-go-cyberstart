91d4e477d7e43f4589b75d78767fcc8a6d16147a
Andres Flores <andres06037@gmail.com>
2020-02-08 16:27:19 -0600

    change

--- Game.java
+++ Game.java
@@ -110,6 +110,7 @@ public class Game {
 
     public static void playRound() {
         Character enemy = makeEnemy();
+        // System.out.println(enemy);
         enemy.setLevel(player.getLevel()-(int)Math.random()*2); // enemy will be same level or 1 level lower
         System.out.println(player.getName() + " is fighting " + enemy.getName());
 
