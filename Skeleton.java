8d3b434507cc52590c02df5a79b22fcae7b17537
Andres Flores <andres06037@gmail.com>
2020-02-07 20:41:16 -0600

    added a weapon

--- Skeleton.java
+++ Skeleton.java
@@ -1,14 +1,15 @@
 public class Skeleton extends Character{
-    private Weapon PoisonSword;
+    private Weapon poisonSword;
 
     public Skeleton(int xp, String weakTo, String name, int howFast){
         super(xp, weakTo, name, howFast);
+        poisonSword = new PoisonSword(25, "Poison", 10);
         
     }
 
     @Override 
     public void chooseAction(Character player){
-        int dmg = PoisonSword.attack(player);
+        int dmg = poisonSword.attack(player);
         System.out.printf("%s was attacked by %s and lost %d hp, and now has %d hp.", player.getName(), this.getName(), dmg, player.getHp()) ;
 
     }
