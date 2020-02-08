0e89d81a7aab3bf6d82ded88e5dff9e3c33dea86
Andres Flores <andres06037@gmail.com>
2020-02-07 19:46:53 -0600

    added main to test in class

--- Archer.java
+++ Archer.java
@@ -12,24 +12,44 @@ public class Archer extends Character {
     @Override
     public void chooseAction(Character target) {
         Scanner s = new Scanner(System.in);
-        System.out.println("What would " + this.getName() + " like to do?");
-        System.out.println("1. Attack \n2. Heal");
         
         String ans = s.nextLine();
 
-        if (ans.equals("1")) {
-            int dam = bow.attack(target);
-            System.out.println(this.getName() + "(" + this.getHp() + ")" + 
-                            " attacked " + target.getName() + "(" + target.getHp() + ")"
-                             + " with " + bow.getWeaponType() + " for " + dam + " damage");
+        while (true) {
+            try {
+                System.out.println("What would " + this.getName() + "(" + this.getHp() + ")" + " like to do?");
+                System.out.println("1. Attack \n2. Heal\n3. Run");
+        
+                if (ans.equals("1")) {
+                    int dam = bow.attack(target);
+                    System.out.println(this.getName() + "(" + this.getHp() + ")" + 
+                                    " attacked " + target.getName() + "(" + target.getHp() + ")"
+                                     + " with " + bow.getWeaponType() + " for " + dam + " damage");
+                }
+                else if (ans.equals("2")) {
+                    int increase = this.getMaxHp();
+                    this.heal((int)(increase * .25));
+                    System.out.println(this.getName() + " healed to " + this.getHp());
+                }
+                else if (ans.equals("3")) {
+                    this.setHp(this.getHp()/2);
+                    System.out.println(this.getName() + " escaped but left with half of its health -> " + this.getHp());
+                }
+                break;
+            }
+            catch (Exception e){
+                System.out.println("Enter in either 1, 2, or 3");
+            }
         }
-        else if (ans.equals("2")) {
-            int increase = this.getMaxHp();
-            this.heal((int)(increase * .25));
-        }
+
 
         s.close();
 
 
+    }
+    public static void main( String[] args) {
+        Character ye = new Archer(500, "Ice", "Andre", 100);
+        Character en = new Ghoul(100, "Piercing", "stinky", 50);
+        ye.chooseAction(en);
     }
 }
